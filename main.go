cccc051c47bc55dd3f6ed044ed9082f3f86923ce
GlitchGuru19 <peterkatebe12@gmail.com>
2025-06-21 22:09:57 +0200

    Using slices to store the details

--- main.go
+++ main.go
@@ -8,32 +8,66 @@ import (
 
 func main(){
 
-	// stored passwords
-	correctUsername := "Carlos"
-	correctPassword := "Falcon12@"
-	
-	// Get user input
-	var (
-		username string
-		password string
-	)
+	// User credentials stored in parallel slices
+	usernames := []string{"admin", "user1", "guest"}
+	passwords := []string{"password123","mypass123","welcome123"}
 
-	fmt.Println("Welcome to the Login System")
-	fmt.Println()
-	fmt.Print("Enter the username: ")
-	fmt.Scanln(&username)
+	// Login attempts
+	maxAttempts := 3
+	attempts := 0
 
-	fmt.Print("Enter the password: ")
-	fmt.Scanln(&password)
+	for attempts < maxAttempts {
+		// Get user input
+		var (
+			inputUser string
+			inputPass string
+		)
 
-	// Check credentials
-	if username == correctUsername && password == correctPassword{
-		fmt.Println("Login successful!")
-	} else if username == correctUsername && password != correctPassword{
-		fmt.Println("Wrong password.")
-	}else if username != correctUsername && password == correctPassword{
-		fmt.Println("No such username.")
-	} else {
-		fmt.Println("Both username and password are incorect.")
+		fmt.Println("Welcome to the Login System")
+		fmt.Println()
+		fmt.Print("Enter the username: ")
+		fmt.Scanln(&inputUser)
+
+		fmt.Print("Enter the password: ")
+		fmt.Scanln(&inputPass)
+
+		// Check credentials 
+		userFound := false
+		correctPassword := false
+		var userIndex int
+
+		// Search for username
+		for i, username := range usernames{
+			if username == inputUser{
+				userFound = true
+				userIndex = i
+				break
+			}
+		}
+
+		// Check password if user is found
+		if userFound {
+			if passwords[userIndex] == inputPass {
+				fmt.Println("\nLogin succesful! Welcome, ", inputUser)
+				return
+			} else {
+				correctPassword = false
+			}
+		}
+
+		// Give appropriate error message
+		switch {
+		case !userFound:
+			fmt.Println("Error: Username not found.")
+		case !correctPassword:
+			fmt.Println("Error: Incorrect Password.")
+		}
+		attempts++
+		remaining := maxAttempts - attempts
+		if remaining > 0 {
+			fmt.Printf("You have %d attempts remaining\n", remaining)
+		} else {
+			fmt.Println("\nMaximum login attempts reached")
+		}
 	}
 }
